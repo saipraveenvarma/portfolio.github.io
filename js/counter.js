f18661061a8d38e7426a9f2a6b58c65b27b1258c
Sai Praveen Varma Mudunuri <saipraveenvarma.mudunuri@gmail.com>
2022-12-27 17:22:38 +0530

    count

--- js/counter.js
+++ js/counter.js
@@ -2,16 +2,17 @@ function updateCounter() {
     const counter = document.getElementById('counter');
     //increment the counter everytime user visits the page by storing in local storage
     let count = localStorage.getItem('visiterCount');
+    console.log(count)
+
+    if(count== String(null)){
+        count = 1;
+    }else(
+        count = paraseInt(count) + 1
+    )
     /**
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
-   if(count == null){
-        count = 1;
-   }
-   else{
-        count += 1;
-   } 
    localStorage.setItem('visiterCount', count);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
 }
